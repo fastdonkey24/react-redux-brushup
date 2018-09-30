257b934a8c9860e20e811b720a5b4bc6f3759d52
Toufiq <burningraven06@gmail.com>
2018-09-30 20:55:36 +0600

    render conditional output

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Gnomes from './components/Gnomes';
 class App extends Component {
   render() {
     const gnomes = [
-      { id : 1, name : "Harry", age : "5"},
-      { id : 2, name : "Marry", age : "6"},
-      { id : 3, name : "Larry", age : "3"},
-      { id : 4, name : "Terry", age : "9"}
+      { id : 1, name : "Harry", age : 5},
+      { id : 2, name : "Marry", age : 6},
+      { id : 3, name : "Larry", age : 3},
+      { id : 4, name : "Terry", age : 9}
     ]
     return (
       <div className="App">
--- src/components/Gnomes.js
+++ src/components/Gnomes.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
 const Gnomes = ({ gnomesList}) => {
-	const TheGnomes = gnomesList.map( gnome => {
-		return (
-			<div key='gnome.id' >
-				<p> Name : {gnome.name}</p>
-				<p> Age : {gnome.age}</p>
-			</div>
-		)
-	})
-
 	return (
 		<div>
-			{TheGnomes}
-		</div>
-		
-	);
+			{gnomesList.map(gnome => {
+				return gnome.age > 5 ? (
+					<div key={gnome.id} id={"gnome_" + gnome.id} >
+						<p> Name : {gnome.name}</p>
+						<p> Age : {gnome.age}</p>
+					</div>
+				) : null
+			})}
+		</div>	
+	)
 }
 
 export default Gnomes;
